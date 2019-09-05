15679dfe2bcc01c8016f4a0d58d43e120ef34573
Johan Steffens (ZA) <johan.steffens@absa.africa>
2019-09-05 11:58:15 +0200

    Initial commit

---
+++ src/test/java/africa/absa/config/makolatest/MakolaTestApplicationTests.java
@@ -0,0 +1,16 @@
+package africa.absa.config.makolatest;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class MakolaTestApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
